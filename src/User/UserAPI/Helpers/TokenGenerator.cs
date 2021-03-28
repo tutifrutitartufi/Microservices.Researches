8f5ca146cd6c41a6706132afd211aaf0b744e6d4
Igor <igor.mirkovicc@gmail.com>
2021-03-28 14:31:35 +0200

    Finishing authentication

--- src/User/UserAPI/Helpers/TokenGenerator.cs
+++ src/User/UserAPI/Helpers/TokenGenerator.cs
@@ -23,7 +23,7 @@ namespace UserAPI.Helpers
             var key = Encoding.ASCII.GetBytes("Zivot tezak nije lak................");
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()), new Claim("role", user.Role.ToString()) }),
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
